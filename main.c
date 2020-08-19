2a9956af889e6c08dc47845f21bd45055fd750be
KoonPorZa <mikuza45956@gmail.com>
2020-08-19 15:19:07 +0000

    โปรแกรมหาพื้นที่รอบทรงกระบอก

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  float r,h,ans;
+  printf("Enter r : ");
+  scanf("%f",&r);
+  printf("Enter h : ");
+  scanf("%f",&h);
+  ans = (2*3.14*r)*h+2*(3.14*(r*r));
+  printf("Answer is %f",ans);
   return 0;
 }
